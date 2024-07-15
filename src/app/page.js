f1d77c25b4b24069be6d424b2ab7bdf4e06f1b2b
Fakhir <itsfakhir2002@gmail.com>
2024-07-15 15:21:59 +0500

    working

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
+// pages/index.js
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Counter from "../components/Counter";
 import UserProfile from "../components/UserProfile";
+import styles from "./Home.module.css"; // Import the CSS module
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <main style={{ paddingLeft: "20px" }}>
+      <main className={styles.mainContent}>
         <h2>Home Page</h2>
         <p>This is the home page.</p>
         <Counter />
